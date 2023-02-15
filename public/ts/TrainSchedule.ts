99a24d186be44321a439d50a5867f5b356f02485
CassHodgkiss <93329378+CassHodgkiss@users.noreply.github.com>
2023-02-15 11:22:18 +0000

    Moving api calls into 3 parts

---
+++ public/ts/TrainSchedule.ts
@@ -0,0 +1,11 @@
+interface TrainSchedule{
+    departure?: string, /* expected departure time */
+    arrival?: string,	/* expected arrival time */
+    pass?: string, 	/* expected pass time */
+    tiploc: string,	/* TIPLOC at location */
+    location: string,	/* Name of location */
+    latLong?: {	
+       latitude: number,
+       longitude: number
+    }
+}
